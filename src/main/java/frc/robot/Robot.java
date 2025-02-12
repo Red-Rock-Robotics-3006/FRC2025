034f49bf68b5a2d13e56a59c5cb7b8a6f34b7d3a
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2025-02-12 15:56:28 -0700

    deleted some random thing epilogue lol

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
